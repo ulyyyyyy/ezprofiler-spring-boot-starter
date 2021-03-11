7a1bef69f761865a24a3eb455290f96f0fbbd26e
Gao zhiwei <zhiweigao@lilith.com>
2021-03-11 18:23:38 +0800

    update MyMongoTemplate

--- src/main/java/com/lilithqa/ezprofiler/repository/MyMongoTemplate.java
+++ src/main/java/com/lilithqa/ezprofiler/repository/MyMongoTemplate.java
@@ -4,6 +4,7 @@ import com.lilithqa.ezprofiler.config.EzProfilerProperties;
 import com.lilithqa.ezprofiler.scanner.AggregateInformation;
 import com.lilithqa.ezprofiler.util.JsonUtils;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
@@ -11,6 +12,7 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import sun.security.krb5.Credentials;
 
 import java.util.ArrayList;
 
@@ -20,26 +22,24 @@ import java.util.ArrayList;
  * @author 黑黑
  * @date 2021-03-10
  */
-@Component
 public class MyMongoTemplate {
 
-    @Autowired
     private MongoClient mongoClient;
 
     private final EzProfilerProperties properties;
 
     public MyMongoTemplate(EzProfilerProperties properties) {
         ServerAddress serverAddress = new ServerAddress(properties.getHost(), properties.getPort());
-        ArrayList<ServerAddress> serverAddresses = new ArrayList<>();
-        ArrayList<MongoCredential> credentials  = new ArrayList<>();
-        serverAddresses.add(serverAddress);
         if (properties.getDbUserName() != null && properties.getDbPassword() != null) {
             MongoCredential credential = MongoCredential.createScramSha1Credential(properties.getDbUserName(),
                     properties.getDataBaseName(), properties.getDbPassword().toCharArray());
-            credentials.add(credential);
+
+            MongoClientOptions build = MongoClientOptions.builder().build();
+            this.mongoClient = new MongoClient(serverAddress, credential, build);
         }
 
-        this.mongoClient = new MongoClient(serverAddresses, credentials);
+        this.mongoClient = new MongoClient(serverAddress);
+        System.out.println("链接链接：：：：：：：：：：：：： " + this.mongoClient.getAddress());
         this.properties = properties;
     }
 
